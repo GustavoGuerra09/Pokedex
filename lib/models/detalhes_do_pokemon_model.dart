92f2699e323ba3d40c619bf510f2d6749f365357
GustavoGuerra09 <gustavoguerra.dev@gmail.com>
2026-05-22 16:54:24 -0300

    image

--- lib/models/detalhes_do_pokemon_model.dart
+++ lib/models/detalhes_do_pokemon_model.dart
@@ -3,12 +3,14 @@ class DetailsPokemonModel {
   final int id;
   final String name;
   final int weight;
+  final String back_default;
 
   DetailsPokemonModel({
     required this.height,
     required this.id,
     required this.name,
-    required this.weight
+    required this.weight,
+    required this.back_default
   });
 
   factory DetailsPokemonModel.fromJson(Map<String, dynamic> json) {
@@ -16,7 +18,8 @@ class DetailsPokemonModel {
       height: json['height'],
       id: json['id'],
       name: json['name'],
-      weight: json['weight']
+      weight: json['weight'],
+      back_default: json['sprites'] ['back_default']
     );
   }
 }
--- lib/pages/detalhes_do_pokemon_page.dart
+++ lib/pages/detalhes_do_pokemon_page.dart
@@ -5,17 +5,22 @@ import 'package:personalizado/models/pokemons_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 // 1 - initstate para chamar a api / tirar botao
 // 2 - loading na tela enquanto api carrega
 // 3 - mudar nomes de varaveis e funcoes
 
 ValueNotifier<DetailsPokemonModel> detalhes =
     ValueNotifier<DetailsPokemonModel>(
-      DetailsPokemonModel(id: 0, height: 0, name: "", weight: 0),
+      DetailsPokemonModel(
+        id: 0,
+        height: 0,
+        name: "",
+        weight: 0,
+        back_default: "",
+      ),
     );
 
-    ValueNotifier<bool> loading = ValueNotifier<bool> (false); 
+ValueNotifier<bool> loading = ValueNotifier<bool>(false);
 
 class DetalhesDoPokemonPage extends StatefulWidget {
   final PokemonModel pokemonClicado;
@@ -37,47 +42,54 @@ class _DetalhesDoPokemonPageState extends State<DetalhesDoPokemonPage> {
 
     final Map<String, dynamic> retorno = jsonDecode(respostaEmformatoDeString);
 
-    DetailsPokemonModel detalhesDePokemon = DetailsPokemonModel.fromJson(retorno);
+    DetailsPokemonModel detalhesDePokemon = DetailsPokemonModel.fromJson(
+      retorno,
+    );
 
     loading.value = false;
 
     detalhes.value = detalhesDePokemon;
   }
 
-@override
-  void  initState() {
+  @override
+  void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       buscaApi();
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.pokemonClicado.name.firstLetterCapitalized)),
+      appBar: AppBar(
+        title: Text(widget.pokemonClicado.name.firstLetterCapitalized),
+      ),
       body: ValueListenableBuilder(
         valueListenable: loading,
-       builder: (context, isLoadingAPI, child) {
-        return Center(
-          child: isLoadingAPI
-          ? Center(child: CircularProgressIndicator())
+        builder: (context, isLoadingAPI, child) {
+          return Center(
+            child: isLoadingAPI
+                ? Center(child: CircularProgressIndicator())
                 : SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              ValueListenableBuilder(
-                valueListenable: detalhes,
-                builder: (_, details, _) {
-                  return details.id == 0 ? SizedBox() : Column(
-                    children: [
-                      Text('ID:${details.id.toString()} '),
-                      Text('Altura::${details.height.toString()}'),
-                      Text('Name::${details.name}'),
-                      Text('o Peso é ${details.weight}')
-                                        
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ValueListenableBuilder(
+                            valueListenable: detalhes,
+                            builder: (_, details, _) {
+                              return details.id == 0
+                                  ? SizedBox()
+                                  : Column(
+                                      children: [
+                                        Text('ID:${details.id.toString()} '),
+                                        Text(
+                                          'Altura::${details.height.toString()}',
+                                        ),
+                                        Text('Name::${details.name}'),
+                                        Text('o Peso é ${details.weight}'),
+                                        Image.network(details.back_default),
                                       ],
                                     );
                             },
